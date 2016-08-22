94b2165b8402425484ea9e611c14ad22c9784bd7
coderman64 <coderman64@users.noreply.github.com>
2016-08-22 11:22:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -107,6 +107,9 @@ function drawAll(){
         }else{
             distance = 0;
             speed *= -0.1;
+            if(speed<0.01&&speed>-0.01){
+                speed = 0;
+            }
         }
         distance -= speed;
         if(speed < 0.01&&speed > -0.01){
